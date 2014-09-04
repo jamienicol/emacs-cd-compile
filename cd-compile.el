10284ccae86afda4a37b09ba90acd1e2efedec9f
Syohei YOSHIDA <syohex@gmail.com>
2014-09-04 12:30:56 +0900

    Take directory as argument to cd-compile.
    When called programmatically take the directory as an argument to
    cd-compile. If the directory doesn't exist print an error message,
    whether called programmatically or interactively.
    
    Fixes #1

--- cd-compile.el
+++ cd-compile.el
@@ -22,16 +22,20 @@
 (put 'cd-compile-directory 'safe-local-variable 'stringp)
 
 ;;;###autoload
-(defun cd-compile()
+(defun cd-compile (dir)
   "Run compile in a specific directory.
-If cd-compile-directory is set then compile will be run in that directory,
-otherwise the user will be prompted to enter a directory."
-  (interactive)
-  (let ((default-directory
-          (if cd-compile-directory
-              (file-name-as-directory cd-compile-directory)
-            (read-directory-name "Compile directory: "))))
-    (call-interactively 'compile)))
+Runs \\[compile] in the directory DIR.
+
+Interactively, uses `cd-compile-directory' for the directory if
+non-nil; otherwise prompts the user to enter the directory."
+  (interactive
+   (list (or cd-compile-directory
+             (read-directory-name "Compile directory: "))))
+  (let ((compile-directory (file-name-as-directory dir)))
+    (if (not (file-directory-p compile-directory))
+        (user-error "%s: no such directory" compile-directory)
+      (let ((default-directory compile-directory))
+        (call-interactively 'compile)))))
 
 (provide 'cd-compile)
 
